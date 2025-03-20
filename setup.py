ca8775709b2a3740653ef86c92b2b3840e5c3d32
D Clayton <dclayton@c5-studios.com>
2025-03-19 21:30:36 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'A GenAI Project',
+    version= '0.0.0',
+    author= 'D Clayton',
+    author_email= 'dclayton@c5-studios.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
